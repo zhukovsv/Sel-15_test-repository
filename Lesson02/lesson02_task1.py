80c0e16c4d5f17532cfafb8ac49d947a9be3c81b
Sergei Zhukov <zhukovsv@tut.by>
2018-02-07 12:43:21 +0300

    Lesson02_task1

---
+++ Lesson02/lesson02_task1.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_1(driver):
+    driver.get("http://www.google.com/")
+    driver.find_element_by_name("q").send_keys("webdriver")
+    driver.find_element_by_name("btnK").click()
+    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Пошук Google"))
