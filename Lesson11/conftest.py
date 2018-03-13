966d74f6db411a32fe7965ff7551502e883a5c75
Sergei Zhukov <zhukovsv@tut.by>
2018-03-13 16:15:27 +0300

    commit delete lesson11

--- Lesson11/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from app.application import Application
-
-
-@pytest.fixture
-def app():
-    appl = Application()
-    yield appl
-    appl.quit()
--- Lesson11/tests/data_providers.py
+++
@@ -1,8 +0,0 @@
-from model.product import Product
-
-
-valid_products = [[],  # No elements
-                  [Product('popular', 'Blue Duck', 1)],  # single product
-                  [Product('popular', 'Blue Duck', 2),  # multiple products
-                   Product('popular', 'Red Duck', 3),
-                   Product('campaigns', 'Yellow Duck', 1, 'Small')]]
